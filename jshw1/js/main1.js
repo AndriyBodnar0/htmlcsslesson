5a37ba2d14089b83f18b87cb3156a2ce5486e3cb
AndriyBodnar0 <bodnarkyzuna@gmail.com>
2021-02-03 01:19:49 +0200

    add pib

--- jshw1/js/main1.js
+++ jshw1/js/main1.js
@@ -1,5 +1,5 @@
 //присвоить каждому из следующих значений свою переменную: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false Вывести каждую при помощи console.log , alert, document.write
-let word = 'hello';
+/* let word = 'hello';
 let kyrs = 'owu';
 let dom = 'com';
 let domain = 'ua';
@@ -15,9 +15,9 @@ let brehn = false;
  console.log(word);
 document.write(word);
 alert(word);
-
+ */
 //перевизначення
-word = 'play';
+/* word = 'play';
 kyrs = 'java';
 dom = 'gov';
 domain = 'ua';
@@ -31,5 +31,23 @@ solodki = 18;
 pravda = 1;
 brehn = 0;
 console.log(word);
-document.write(word);
-alert(word);
+document.write(a);
+alert(c); */
+//створити 3 const числові і техтові
+/* const oneoneone = 111;
+const min = -0.256;
+const max = 65535;
+const save = 'sd';
+const waga = 'weight';
+console.log(min);
+document.write(save);
+alert(waga); */
+//ПІБ promt
+let mypib =  prompt('Боднар Андрій Андрійович');
+console.log(mypib);
+
+
+
+
+
+

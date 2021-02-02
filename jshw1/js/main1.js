6340b765362ed290d2986c57a4fb5b542122a158
AndriyBodnar0 <bodnarkyzuna@gmail.com>
2021-02-03 00:52:50 +0200

    first commit

---
+++ jshw1/js/main1.js
@@ -0,0 +1,35 @@
+//присвоить каждому из следующих значений свою переменную: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false Вывести каждую при помощи console.log , alert, document.write
+let word = 'hello';
+let kyrs = 'owu';
+let dom = 'com';
+let domain = 'ua';
+let one = 1;
+let dec = 10;
+let a = -999;
+let b = 123;
+let pi = 3.14;
+let c = 2.7;
+let solodki = 16;
+let pravda = true;
+let brehn = false;
+ console.log(word);
+document.write(word);
+alert(word);
+
+//перевизначення
+word = 'play';
+kyrs = 'java';
+dom = 'gov';
+domain = 'ua';
+one = 11;
+dec = 20;
+a = -65535;
+b = 255;
+pi = 4644;
+c = 45.5566;
+solodki = 18;
+pravda = 1;
+brehn = 0;
+console.log(word);
+document.write(word);
+alert(word);
